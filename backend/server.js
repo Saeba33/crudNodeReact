f04f68ebb367a9d4161dff11f2570146fc739de6
Charles Proust <konoha33@hotmail.fr>
2025-09-30 15:57:21 +0200

    create connection to database

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
 
-app.listen(8081, () => {
-	console.log("Server is running on port 8081");
-});
+
 
 const corsOptions = {
 	origin: ["http://localhost:3000", "http://localhost:8081"],
@@ -15,4 +13,23 @@ const corsOptions = {
 	credentials: true,
 };
 
-mysql.createConnection
+const database = mysql.createConnection({
+	host: "localhost",
+	port: 8889,
+	user: "root",
+	password: "root",
+	database: "crudnode",
+});
+
+app.get("/", (req, res) => {
+	const sql= "SELECT * FROM students";
+    database.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data)
+    })
+});
+
+
+app.listen(8081, () => {
+	console.log("Server is running on port 8081");
+});
