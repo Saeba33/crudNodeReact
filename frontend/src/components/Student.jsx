2f5bf2ca298d10b4d89c91239d31ba22c914d494
Charles Proust <konoha33@hotmail.fr>
2025-10-01 10:54:34 +0200

    install axios

--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Student() {
-	return <div className="text-red-500">Hello World</div>;
+
+	const [student, setStudent] = useState([]);
+
+	useEffect(() => {
+		axios.get('http://localhost:8081')
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+	}, [])
+
+
+	return (
+		<div className="text-red-500">
+			Hello World
+			<div>
+				<button>Ajouter</button>
+				<table>
+					<thead>
+						<tr>
+							<th>Nom</th>
+							<th>Email</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							student.map((data, index) => (
+								<tr key={index}>
+									<td>{data.name}</td>
+									<td>{DataTransfer.email}</td>
+								</tr>
+							))
+						}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
 
 export default Student;
